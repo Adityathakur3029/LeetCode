3d7018fb76ef4935e7f1f715b64c6acf3322f7a5
Adityathakur3029 <44410456+Adityathakur3029@users.noreply.github.com>
2020-05-07 04:37:24 -0700

    Create cousinsInBinaryTree.cpp

---
+++ cousinsInBinaryTree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        int foundVal;                                 //x or y
+        
+        queue<TreeNode*> q;
+        bool flag=false;                              //turn true when either x or y is found
+        
+        q.emplace(root);
+        
+        
+        while(!q.empty()){
+            int size=q.size();
+            
+            while(size--){                              // one level
+                TreeNode* curr= q.front();
+                q.pop();
+                if(curr->left && curr->right){          //check they are not the child of same parent
+                    if((curr->left->val==x && curr->right->val==y) || (curr->right->val==x && curr->left->val==y))
+                        return false;
+                }
+                
+                
+                if(flag){                             //one of the val have found checking for other
+                    if((curr->val==x || curr->val==y) && curr->val!=foundVal )
+                        return true;
+                }
+                
+                if(!flag){                             //found first val 
+                    if(curr->val==x || curr->val==y){  //turn flag on and set foundVal
+                        flag=true;
+                        foundVal=curr->val;
+                    }
+                }
+                
+                if(curr->left)
+                    q.emplace(curr->left);
+                if(curr->right)
+                    q.emplace(curr->right);
+            }
+            if(flag)                                // one val found but other was missing in same level
+                return false;
+        }
+        
+        return false;
+    }
+};
