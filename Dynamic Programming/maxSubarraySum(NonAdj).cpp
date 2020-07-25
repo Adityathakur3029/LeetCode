d32f0383c9039a4e7dddfbcbd909646092f52fce
Adityathakur3029 <44410456+Adityathakur3029@users.noreply.github.com>
2020-07-25 03:28:22 -0700

    Update maxSubarraySum(NonAdj).cpp

--- Dynamic Programming/maxSubarraySum(NonAdj).cpp
+++ Dynamic Programming/maxSubarraySum(NonAdj).cpp
@@ -42,3 +42,20 @@ public:
         return sum;
     }
 };
+
+
+public static int maxSumSubSeqNonContagious(int a[]){
+	int max_include[] = new int[a.length];
+	int max_exclude[] = new int[a.length];
+	max_include[0] = a[0];
+	max_exclude[0] = Integer.MIN_VALUE;
+	int max = a[0];
+	
+	for(int i = 1; i<a.length; i++){
+		max_include[i] = Math.max(max_exclude[i-1]+a[i], a[i]);
+		max_exclude[i] = Math.max(max_include[i-1], max_exclude[i-1]);
+		max = Math.max(max_include[i], max_exclude[i]);
+	}
+	
+	return max;
+}
