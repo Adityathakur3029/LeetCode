6ba3a3be924021e7ecc7fc3dba48d94742019cf9
Adityathakur3029 <44410456+Adityathakur3029@users.noreply.github.com>
2020-08-05 10:41:15 -0700

    Update rightViewofTree.cpp

--- Trees/Questions/rightViewofTree.cpp
+++ Trees/Questions/rightViewofTree.cpp
@@ -43,3 +43,25 @@ public:
 
 
 //we can perform level order as well
+
+//recursive
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        //using info=pair<int,TreeNode*>; 
+        
+        vector<int> res;
+        helper(root,0,res);
+        return res;
+    }
+    
+    void helper(TreeNode* root, int level, vector<int>& res){
+        if(!root)
+            return;
+        if(level==res.size())
+            res.push_back(root->val);
+        helper(root->right,level+1,res);
+        
+        helper(root->left,level+1,res);
+    }
+};
